f5b2b5ba9fa126e3d8fca33a094a509c50423812
felipevargas-bz <felipe15angel@gmail.com>
2021-05-29 15:11:53 -0500

    Version 1.0

---
+++ billing_program.py
@@ -0,0 +1,41 @@
+class Product:
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+def add_product(list):
+    string = input("Add new product: ")
+    yes = 'yes'
+    no = 'no'
+
+    tokens = string.split(sep='= ')
+
+    try:
+        item = Product(tokens[0], int(tokens[1]))
+        list.append(item)
+        state = True
+    except:
+        state = False
+        raise ValueError("<Error> You must enter correct values")
+    if state is False:
+        return add_product(list)
+    else:
+        print("Do you want to continue billing? (yes or no)")
+        buy = input()
+
+        if buy == yes:
+            return add_product(list)
+        else:
+            index = 0
+            while (index < len(list)):
+                print("name: {}, price: {}".format(list[index].name, list[index].value))
+                index += 1
+
+def main():
+    list = []
+    print("Please enter the products to be invoiced. do it as follows: (product_name = price).")
+    add_product(list)
+
+if __name__ == "__main__":
+    main()
+
