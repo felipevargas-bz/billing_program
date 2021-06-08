0a3e4704050a6cf944fc84765ef16cc0e4033e30
felipevargas-bz <felipe15angel@gmail.com>
2021-06-08 17:56:52 -0500

    Delete

--- front.py
+++ front.py
@@ -11,7 +11,7 @@ def add():
     p_list.append(add_product(name.get(), price.get()))
     t_invoice.set(str(total_invoice()))
     t_iva.set(str(total_iva()))
-    t_discouts.set(str(total_discounts()))
+   # t_discouts.set(str(total_discounts()))
 
 
 
